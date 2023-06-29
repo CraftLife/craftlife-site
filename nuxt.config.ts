3273451c7f51f2b28a38c095df62888cc24285d9
Bruno Rodrigues <github@brunodsr.com>
2023-06-29 20:29:11 -0300

    Adiciona pagamento

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,12 +14,13 @@ export default defineNuxtConfig({
             'Servidor de Minecraft Brasileiro, Servidor de Minecraft 1.20.1, Survival, PVP, Creative, EggWars, SkyWars, SkyBlock, KitPVP, Como jogar Minecraft Online, Servidor de minecraft ultima versao, servidor mcpe brasileiro'
         }
       ],
-      script: ['https://unpkg.com/@phosphor-icons/web']
+      script: ['https://unpkg.com/@phosphor-icons/web', 'https://sdk.mercadopago.com/js/v2']
     }
   },
   runtimeConfig: {
     public: {
-      baseURL: process.env.BASE_URL || 'http://localhost:8080'
+      baseURL: process.env.BASE_URL || 'http://localhost:8080',
+      mercadopagoPublicKey: 'APP_USR-4b1cbcc0-6b15-4984-af69-4832390959ca'
     }
   },
   devtools: { enabled: true },
@@ -32,8 +33,5 @@ export default defineNuxtConfig({
       }
     },
     baseURL: 'http://localhost:8080/auth'
-    // globalAppMiddleware: {
-    //   isEnabled: true,
-    // }
   }
 })
--- pages/store.vue
+++ pages/store.vue
@@ -15,6 +15,7 @@
           <img src="@/assets/images/vip_ouro.png" alt="" />
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = '5ab3bc1c-e366-4b87-a460-90ce763a6bef'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_ouro.png" width="100" />
@@ -24,6 +25,7 @@
           <img src="@/assets/images/vip_diamante.png" alt="" />
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = 'ecfd20bf-080c-4c08-a8be-e0432085da14'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_diamante.png" width="100" />
@@ -33,6 +35,7 @@
           <img src="@/assets/images/vip_netherite.png" alt="" />
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = '15df1816-85d8-497b-b941-baf02502afed'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_netherite.png" width="100" />
@@ -81,6 +84,7 @@
         <div class="p-4">
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = '5ab3bc1c-e366-4b87-a460-90ce763a6bef'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_ouro.png" width="100" />
@@ -89,6 +93,7 @@
         <div class="p-4">
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = 'ecfd20bf-080c-4c08-a8be-e0432085da14'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_diamante.png" width="100" />
@@ -97,6 +102,7 @@
         <div class="p-4">
           <button
             class="flex flex-col items-center bg-green-200 hover:bg-green-300 hover:scale-105 transition duration-150 ease-in-out px-4 py-2 rounded-md shadow-md w-full"
+            @click="selectedProduct = '15df1816-85d8-497b-b941-baf02502afed'"
           >
             <span v-text="'Comprar agora por'" />
             <img src="@/assets/images/preco_netherite.png" width="100" />
@@ -104,9 +110,58 @@
         </div>
       </div>
     </div>
+    <div v-show="modalVisible" class="fixed inset-0 bg-black bg-opacity-20 flex justify-center items-center">
+      <div class="relative bg-white px-8 py-4 rounded-xl shadow-xl">
+        <button class="absolute top-2 right-2" @click="modalVisible = false">
+          <i class="ph ph-x text-red-400 text-xl" />
+        </button>
+        <h2 class="mb-3">Por favor, insira seu nick para continuar</h2>
+        <input
+          class="border-2 border-pink-500 rounded-md px-4 py-1 focus:outline-none w-full mb-3"
+          type="text"
+          v-model="username"
+        />
+        <button
+          class="flex items-center space-x-2 text-white rounded-md bg-pink-400 hover:bg-pink-500 px-4 py-1"
+          @click="openCheckoutModal()"
+        >
+          <span>Continuar</span>
+          <i class="ph ph-arrow-right"></i>
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script lang="ts" setup>
 import beneficiosSessoes from '@/data/vantagens-vip.json'
+
+const config = useRuntimeConfig()
+
+// @ts-ignore
+const mp = new MercadoPago(config.public.mercadopagoPublicKey)
+const selectedProduct = ref('')
+const modalVisible = ref(false)
+const username = ref('')
+
+watch(selectedProduct, () => {
+  modalVisible.value = true
+})
+
+async function openCheckoutModal() {
+  modalVisible.value = false
+  const { pending, data } = await useApiFetch('/checkout', {
+    method: 'POST',
+    body: {
+      username: username.value,
+      product_id: selectedProduct.value
+    }
+  })
+  mp.checkout({
+    preference: {
+      id: data.value.mercadopago_preference_id
+    },
+    autoOpen: true
+  })
+}
 </script>
