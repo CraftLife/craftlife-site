52c7ebe4bb191c6e46bb15596bdcf998d9bb5f19
Bruno <bds.rodrigues@icloud.com>
2024-05-10 21:54:42 -0300

    Homolog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,13 +7,44 @@
       >
         <h2 class="m-0 text-4xl bg-black-alpha-60 border-round-md p-2">DOE PARA NOSSOS COLEGAS DO RS!</h2>
         <p class="m-0 text-xl bg-black-alpha-60 border-round-md p-2 max-w-30rem">
-          Cada produto comprado em nossa loja até 30/05, 100% do dinheiro arrecadado será doado para ajudar as familías
+          Cada produto comprado em nossa loja até 30/05, 100% do dinheiro arrecadado será doado para ajudar as famílias
           do rio grande do sul!
         </p>
       </RouterLink>
     </template>
   </Card>
 
+  <div class="grid mt-2">
+    <div class="col-6">
+      <Card class="h-full">
+        <template #title>Meta de doação</template>
+        <template #content>
+          <ProgressBar class="mt-4" :value="(120 / 300) * 100">{{ (120 / 300) * 100 }}%</ProgressBar>
+          <div class="mt-4">
+            Foram doados <span class="text-green-500">R$ 120,00</span> até o momento, a meta é
+            <span class="text-green-500">R$ 300,00</span>
+          </div>
+        </template>
+      </Card>
+    </div>
+    <div class="col-6">
+      <Card class="h-full">
+        <template #title>Últimas doações</template>
+        <template #content>
+          <div class="flex flex-column gap-2">
+            <div class="flex align-items-center">
+              <img class="w-3rem h-3rem border-round" src="https://mineskin.eu/helm/Brun0XP/64.svg" alt="" />
+              <div class="ml-3">
+                <p class="m-0">Brun0XP</p>
+                <p class="m-0">VIP NETHERITE</p>
+              </div>
+              <div class="ml-auto text-xl text-green-500">R$ 35,00</div>
+            </div>
+          </div>
+        </template>
+      </Card>
+    </div>
+  </div>
   <Card class="mt-2">
     <template #header>
       <div class="flex align-items-center gap-3 border-dashed border-none border-bottom-2 surface-border p-4">
@@ -61,5 +92,7 @@
 .banner {
   background: url('@/assets/imgs/campanha-rs.jpeg');
   background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
